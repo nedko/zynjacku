eb2695e535fbad50c0778527b204167bbcedc54c
Nedko Arnaudov <nedko@arnaudov.name>
2008-12-10 01:33:07 +0200

    Refactor & cleanup

--- midiccmap.py
+++ midiccmap.py
@@ -24,13 +24,16 @@ import calfwidgets
 class midiccmap:
     def __init__(self, parameter_name, cc_no, min_value=0.0, max_value=1.0, points=[]):
         self.parameter_name = parameter_name
+        self.min_value = min_value
+        self.max_value = max_value
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        #self.window.set_size_request(650,650)
 
+        # the grand vbox
         vbox = gtk.VBox()
         vbox.set_spacing(5)
         self.window.add(vbox)
 
+        # top hbox
         hbox_top = gtk.HBox()
         hbox_top.set_spacing(10)
         align = gtk.Alignment(0.5, 0.5, 1.0, 1.0)
@@ -38,6 +41,21 @@ class midiccmap:
         align.add(hbox_top)
         vbox.pack_start(align, False)
 
+        self.adj_cc_no = gtk.Adjustment(cc_no, 0, 127, 1, 19)
+        self.adj_cc_no.connect("value-changed", self.on_cc_no_changed)
+        cc_no = gtk.SpinButton(self.adj_cc_no, 0.0, 0)
+        cc_no_box = gtk.HBox()
+        cc_no_box.pack_start(gtk.Label("MIDI CC #"))
+        cc_no_box.pack_start(cc_no)
+        hbox_top.pack_start(cc_no_box, False)
+
+        hbox_top.pack_start(gtk.Label("Show parameter range:"), False)
+        button = gtk.RadioButton(None, "mapped")
+        hbox_top.pack_start(button, False)
+        button = gtk.RadioButton(button, "full")
+        hbox_top.pack_start(button, False)
+
+        # middle hbox
         hbox_middle = gtk.HBox()
         hbox_middle.set_spacing(10)
         align = gtk.Alignment(0.5, 0.5, 1.0, 1.0)
@@ -45,22 +63,15 @@ class midiccmap:
         align.add(hbox_middle)
         vbox.pack_start(align)
 
-        vbox_top_left = gtk.VBox()
-        vbox_top_left.set_spacing(5)
-        hbox_middle.pack_start(vbox_top_left, False)
+        vbox_left = gtk.VBox()
+        vbox_left.set_spacing(5)
+        hbox_middle.pack_start(vbox_left, False)
 
-        curve = gtk.Frame("Here will be the curve widget..................................")
-        curve.set_size_request(250,250)
-        hbox_middle.pack_start(curve, True, True)
-
-        start_value_text = "Start value"
-        end_value_text = "End value"
-
-        self.ls = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gtk.Adjustment, str, bool, str)
+        self.ls = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gtk.Adjustment, bool, str)
 
         r = gtk.CellRendererText()
         c1 = gtk.TreeViewColumn("From", r, text=0)
-        c2 = gtk.TreeViewColumn("->", r, text=5)
+        c2 = gtk.TreeViewColumn("->", r, text=4)
         c3 = gtk.TreeViewColumn("To", r, text=1)
 
         self.tv = gtk.TreeView(self.ls)
@@ -80,8 +91,25 @@ class midiccmap:
         sw.add(self.tv)
         tv_box.pack_start(sw)
 
-        vbox_top_left.pack_start(tv_box, True, True)
+        vbox_left.pack_start(tv_box, True, True)
 
+        self.cc_value_delete_button = gtk.Button("Remove")
+        self.cc_value_delete_button.connect("clicked", self.on_button_clicked)
+        vbox_left.pack_start(self.cc_value_delete_button, False)
+
+        self.cc_value_new_button = gtk.Button("New")
+        self.cc_value_new_button.connect("clicked", self.on_button_clicked)
+        vbox_left.pack_start(self.cc_value_new_button, False)
+
+        self.cc_value_change_button = gtk.Button("Change CC value")
+        self.cc_value_change_button.connect("clicked", self.on_button_clicked)
+        vbox_left.pack_start(self.cc_value_change_button, False)
+
+        curve = gtk.Frame("Here will be the curve widget..................................")
+        curve.set_size_request(250,250)
+        hbox_middle.pack_start(curve, True, True)
+
+        # bottom hbox
         hbox_bottom = gtk.HBox()
         hbox_bottom.set_spacing(10)
         align = gtk.Alignment(0.5, 0.5, 1.0, 1.0)
@@ -89,65 +117,32 @@ class midiccmap:
         align.add(hbox_bottom)
         vbox.pack_start(align, False)
 
-        self.adj_cc_no = gtk.Adjustment(cc_no, 0, 127, 1, 19)
-        self.adj_cc_no.connect("value-changed", self.on_cc_no_changed)
-        cc_no = gtk.SpinButton(self.adj_cc_no, 0.0, 0)
-        cc_no_box = gtk.HBox()
-        cc_no_box.pack_start(gtk.Label("MIDI CC #"))
-        cc_no_box.pack_start(cc_no)
-        #cc_frame = gtk.Frame()
-        #cc_frame.add(cc_box)
-        hbox_top.pack_start(cc_no_box, False)
+        self.adj_cc_value = gtk.Adjustment(17, 0, 127, 1, 10)
+        self.adj_cc_value.connect("value-changed", self.on_cc_value_changed)
 
-        hbox_top.pack_start(gtk.Label("Show parameter range:"), False)
-        button = gtk.RadioButton(None, "mapped")
-        hbox_top.pack_start(button, False)
-        button = gtk.RadioButton(button, "full")
-        hbox_top.pack_start(button, False)
-
-        self.adj_cc_value = gtk.Adjustment(17, 0, 127, 1, 19)
-        self.cc_value = gtk.SpinButton(self.adj_cc_value, 0.0, 0)
+        self.cc_value = gtk.SpinButton(None, 0.0, 0)
+        self.cc_value.set_adjustment(self.adj_cc_value)
         label = gtk.Label("CC value")
         hbox_bottom.pack_start(label, False)
         hbox_bottom.pack_start(self.cc_value, False)
 
-        self.cc_value_delete_button = gtk.Button("Remove")
-        vbox_top_left.pack_start(self.cc_value_delete_button, False)
-        self.cc_value_delete_button.connect("clicked", self.on_button_clicked)
-
-        self.cc_value_new_button = gtk.Button("New")
-        vbox_top_left.pack_start(self.cc_value_new_button, False)
-        self.cc_value_new_button.connect("clicked", self.on_button_clicked)
-
-        self.cc_value_change_button = gtk.Button("Change CC value")
-        vbox_top_left.pack_start(self.cc_value_change_button, False)
-        self.cc_value_change_button.connect("clicked", self.on_button_clicked)
-
-        self.adj_cc_value.connect("value-changed", self.on_cc_value_changed)
+        hbox_bottom.pack_start(gtk.Label("-> %s" % self.parameter_name), False)
 
         self.value = calfwidgets.Knob()
-        self.value_label = gtk.Label()
-        hbox_bottom.pack_start(self.value_label, False)
         hbox_bottom.pack_start(self.value, False)
+
         self.value_spin = gtk.SpinButton(digits=2)
         hbox_bottom.pack_start(self.value_spin, False)
 
-        iter = self.ls.append(["0", "", adj_min, "MIDI CC value 0", True, "->"])
-        adj_min.connect("value-changed", self.on_value_changed, iter)
-        self.on_value_changed(None, iter)
-
-        for point in points:
+        # add points
+        self.new_point(points[0][0], points[0][1], True)
+        for point in points[1:-1]:
             self.new_point(point[0], point[1])
-
-        iter = self.ls.append(["127", "", adj_max, "MIDI CC value 127", True, "->"])
-        adj_max.connect("value-changed", self.on_value_changed, iter)
-        self.on_value_changed(None, iter)
+        self.new_point(points[-1][0], points[-1][1], True)
 
         self.tv.get_selection().connect("changed", self.on_selection_changed)
 
-        self.current_row = (0,)
-        self.current_immutable = True
-        self.tv.get_selection().select_path(self.current_row)
+        self.tv.get_selection().select_path((0,))
 
         self.set_title()
 
@@ -171,18 +166,16 @@ class midiccmap:
             return
         row = self.ls[iter]
         #print "selected %s" % row[0]
-        #self.value_label.set_text(row[3])
-        self.value_label.set_text("-> %s" % self.parameter_name)
+
+        # is immutable?
+        immutable = row[3]
+        self.cc_value_delete_button.set_sensitive(not immutable)
+        self.current_immutable = immutable
+
         self.value.set_adjustment(row[2])
         self.value_spin.set_adjustment(row[2])
         self.value_spin.set_value(row[2].value)
         self.cc_value.set_value(int(row[0]))
-
-        # is immutable?
-        immutable = row[4]
-        self.cc_value_delete_button.set_sensitive(not immutable)
-
-        self.current_immutable = immutable
 
         self.on_cc_value_changed(row[2])
 
@@ -196,7 +189,7 @@ class midiccmap:
         self.cc_value_new_button.set_sensitive(True)
         self.cc_value_change_button.set_sensitive(not self.current_immutable)
 
-    def new_point(self, cc_value, value):
+    def new_point(self, cc_value, value, immutable=False):
         #print "new point %u" % cc_value
         prev_iter = None
 
@@ -205,8 +198,8 @@ class midiccmap:
                 break
             prev_iter = row.iter
         
-        adj = gtk.Adjustment(value, 0, 1, 0.01, 0.2)
-        iter = self.ls.insert_after(prev_iter, [str(cc_value), "", adj, "MIDI CC value %u" % cc_value, False, "->"])
+        adj = gtk.Adjustment(value, self.min_value, self.max_value, 0.01, 0.2)
+        iter = self.ls.insert_after(prev_iter, [str(cc_value), "", adj, immutable, "->"])
         adj.connect("value-changed", self.on_value_changed, iter)
         self.on_value_changed(None, iter)
         return iter
@@ -230,13 +223,15 @@ class midiccmap:
             selection.select_path(path)
 
 values = [
+    [0, 0.1],
     [1, 0.12],
     [2, 0.13],
     [3, 0.14],
     [4, 0.18],
     [5, 0.2],
     [56, 0.71],
-    [89, 0.80]
+    [89, 0.80],
+    [127, 0.95]
     ]
-m = midiccmap("Modulation", 23, 0.1, 0.9, values)
+m = midiccmap("Modulation", 23, 0, 1, values)
 gtk.main()
