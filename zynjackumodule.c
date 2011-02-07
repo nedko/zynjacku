e24dd822dd78f68edfe70a98142379e0c4878d42
Nedko Arnaudov <nedko@arnaudov.name>
2011-02-07 20:56:58 +0200

    Fix build on OSX. Patch by Robin Gareus.

--- zynjackumodule.c
+++ zynjackumodule.c
@@ -19,7 +19,7 @@
  *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  *****************************************************************************/
-
+#define NO_IMPORT_PYGOBJECT
 #include <pygobject.h>
  
 void zynjacku_c_register_classes(PyObject * obj);
